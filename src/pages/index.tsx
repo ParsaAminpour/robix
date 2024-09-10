72532ab5f630b914862a74d30183f841f75927fb
Teo kamalipour <teoakamalipour@gmail.com>
2024-09-10 19:27:45 +0330

    feat: prettier and pre-commit added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import Head from "next/head";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Robix</title>
-        <meta name="description" content="" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Hello Robix</h1>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Robix</title>
+				<meta
+					name="description"
+					content=""
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<link
+					rel="icon"
+					href="/favicon.ico"
+				/>
+			</Head>
+			<h1>Hello Robix</h1>
+		</>
+	);
 }
