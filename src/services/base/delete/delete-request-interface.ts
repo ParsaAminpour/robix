23069fd2717b3caf90333b0e00f96875b3a2315e
Teo kamalipour <teoakamalipour@gmail.com>
2024-09-10 19:59:57 +0330

    feat: api services base added

---
+++ src/services/base/delete/delete-request-interface.ts
@@ -0,0 +1,3 @@
+import { AxiosRequestConfig } from "axios";
+
+export interface IDeleteRequestOption<D> extends Omit<AxiosRequestConfig<D>, "method"> {}
--- src/types/global.types.ts
+++ src/types/global.types.ts
@@ -1,3 +1,3 @@
 import { PropsWithChildren } from "react";
 
-export interface BaseProps<T = object> extends React.FC<PropsWithChildren<T>> {}
+export interface BaseProps<T = unknown> extends React.FC<PropsWithChildren<T>> {}
